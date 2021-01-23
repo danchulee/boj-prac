cdea1528091706595a6e169208d273976030fa35
danchulee <danchulee@naver.com>
2021-01-23 14:34:07 +0900

    before MODIFIED

--- GreedyAlgo/src/algo/boj/Prob1339.java
+++ GreedyAlgo/src/algo/boj/Prob1339.java
@@ -3,7 +3,7 @@ package algo.boj;
 import java.util.*;
 
 public class Prob1339 {
-    static int N;
+    static int N, ans;
     static String[] words;
 
     public static void main(String[] args) {
@@ -23,6 +23,7 @@ public class Prob1339 {
             Map<Character, Integer> map = new HashMap<>();
             Pair max_key = new Pair();
             for (int j = 0; j < N; j++) { // 개수 세는 중 (가장 많이 나온 거 찾아야 함)
+                if (words[j].charAt(i) == ' ') continue;
                 if (map.containsKey(words[j].charAt(i))) {
                     map.replace(words[j].charAt(i), map.get(words[j].charAt(i)) + 1);
                 } else map.put(words[j].charAt(i), 1);
@@ -31,15 +32,25 @@ public class Prob1339 {
                     max_key.letter = words[j].charAt(i);
                 }
             }
+            // 한 자리당 하나만 부여할 게 아니라 최대한 많이 부여해줘야 한다.
             // ... 알파벳에 숫자 지정
-            for (int j = 9; i >= 0; i--) {
-                if (letters[i] == '\u0000' && !used.containsKey(max_key.letter)) {
-                    letters[i] = max_key.letter;
-                    used.put(letters[i], 1);
+            if (max_key.appears == 0) continue;
+            for (int j = 9; j >= 0; j--) {
+                if (letters[j] == '\u0000' && !used.containsKey(max_key.letter)) {
+                    letters[j] = max_key.letter;
+                    used.put(letters[j], 1);
+                    break;
                 }
             }
         }
-        System.out.println("일단락");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 10; j++)
+                if (letters[j] != '\u0000')
+                    words[i] = words[i].replace(Character.toString(letters[j]), Integer.toString(j));
+            ans += Integer.parseInt(words[i].trim());
+        }
+        System.out.println(ans);
+        sc.close();
         // ... 연산
 
     } // end of Main
