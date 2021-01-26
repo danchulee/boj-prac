391fe1c14d11aaf19a9d67f902112f85fda7a8e0
danchulee <danchulee@naver.com>
2021-01-26 22:41:50 +0900

    ADD prob12904

--- GreedyAlgo/src/algo/boj/Prob12904.java
+++ GreedyAlgo/src/algo/boj/Prob12904.java
@@ -1,4 +1,28 @@
 package algo.boj;
 
+import java.util.Scanner;
+
 public class Prob12904 {
-}
+    static int ans = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.next();
+        String T = sc.next();
+
+        while (true) {
+            if (T.length() == S.length()) {
+                if (S.equals(T))
+                    ans = 1;
+                break;
+            }
+            char last = T.charAt(T.length() - 1);
+            T = T.substring(0, T.length() - 1);
+            if (last == 'B')
+                T = new StringBuffer(T).reverse().toString();
+        }
+        System.out.println(ans);
+        sc.close();
+    } // end of Main
+} // end of Class
+
