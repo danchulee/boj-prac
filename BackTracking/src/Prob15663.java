b000b1a58fdc7f43bcfd8e36aa2ccfa7caa4f8c9
danchulee <danchulee@naver.com>
2021-02-07 21:21:09 +0900

    ADD

---
+++ BackTracking/src/Prob15663.java
@@ -0,0 +1,2 @@
+public class Prob15663 {
+}
---
+++ Queue/src/Prob2164.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Prob2164 {
+    static Queue<Integer> q = new LinkedList<Integer>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++)
+            q.offer(i);
+
+        while (q.size() != 1) {
+            q.poll();
+            if(q.size() == 1) break;
+            q.offer(q.poll());
+        }
+
+        System.out.println(q.poll());
+    }
+}
