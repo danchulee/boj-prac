ef1c293b2fbfe4e4037db41d20b428cced040ece
danchulee <danchulee@naver.com>
2021-05-04 22:48:19 +0900

    ADD prob2631

--- src/Prob2631_줄세우기.java
+++ src/Prob2631_줄세우기.java
@@ -1,8 +1,26 @@
 import java.io.*;
 import java.util.*;
 
+// 증가하는 가장 긴 수열 lis
 public class Prob2631_줄세우기 {
-    public static void main(String[] args) {
+    static int N, max;
+    static int[] tall;
+    static int[] DP;
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        tall = new int[N];
+        DP = new int[N];
+        for (int i = 0; i < N; i++)
+            tall[i] = Integer.parseInt(br.readLine());
+        Arrays.fill(DP, 1);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++)
+                if (tall[i] > tall[j])
+                    DP[i] = Math.max(DP[i], DP[j] + 1);
+            max = Math.max(max, DP[i]);
+        }
+        System.out.print(N - max);
     }
 }
