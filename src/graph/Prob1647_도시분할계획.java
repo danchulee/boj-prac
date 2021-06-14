5ca295ccce3be90dd7694ceda6d4bce8eb23195b
danchulee <danchulee@naver.com>
2021-06-14 14:34:33 +0900

    ADD prob1647 크루스칼 추가

--- src/graph/Prob1647_도시분할계획.java
+++ src/graph/Prob1647_도시분할계획.java
@@ -5,13 +5,25 @@ import java.util.*;
 
 public class Prob1647_도시분할계획 {
     static int N, M;
-    static ArrayList<Node>[] graph;
+    static int[] parent;
+    //    static ArrayList<Node>[] graph;
+//
+//    static class Node {
+//        int vertex, dist;
+//
+//        Node(int vertex, int dist) {
+//            this.vertex = vertex;
+//            this.dist = dist;
+//        }
+//    }
+    static Edge[] edgeList;
 
-    static class Node {
-        int vertex, dist;
+    static class Edge {
+        int a, b, dist;
 
-        Node(int vertex, int dist) {
-            this.vertex = vertex;
+        Edge(int a, int b, int dist) {
+            this.a = a;
+            this.b = b;
             this.dist = dist;
         }
     }
@@ -22,39 +34,66 @@ public class Prob1647_도시분할계획 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        graph = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
-
+//        graph = new ArrayList[N + 1];
+//        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) parent[i] = i;
+        edgeList = new Edge[M];
         int a, b, dist;
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             a = Integer.parseInt(st.nextToken());
             b = Integer.parseInt(st.nextToken());
             dist = Integer.parseInt(st.nextToken());
-            graph[a].add(new Node(b, dist));
-            graph[b].add(new Node(a, dist));
+            edgeList[i] = new Edge(a, b, dist);
+//            graph[a].add(new Node(b, dist));
+//            graph[b].add(new Node(a, dist));
         }
-        System.out.print(prim());
+        Arrays.sort(edgeList, (o1, o2) -> o1.dist - o2.dist);
+        int cnt = 0, max = 0, ans = 0;
+        for (Edge edge : edgeList)
+            if (union(edge.a, edge.b)) {
+                ans += edge.dist;
+                max = Math.max(max, edge.dist);
+                if (++cnt == N - 1) break;
+            }
+        System.out.print(ans - max);
+//        System.out.print(prim());
     }
 
-    public static int prim() {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        pq.offer(new int[]{1, 0});
+    public static boolean union(int a, int b) {
+        int aRoot = find(a);
+        int bRoot = find(b);
+        if (aRoot == bRoot) return false;
+        else if (aRoot < bRoot) parent[bRoot] = aRoot;
+        else parent[aRoot] = bRoot;
+        return true;
 
-        int max = 0;
-        int cnt = 0, res = 0;
-        boolean[] visit = new boolean[N + 1];
-        int[] curr;
-        while (!pq.isEmpty()) {
-            curr = pq.poll();
-            if (visit[curr[0]]) continue;
-            visit[curr[0]] = true;
-            max = Math.max(max, curr[1]);
-            res += curr[1];
-            for (Node node : graph[curr[0]])
-                if (!visit[node.vertex]) pq.offer(new int[]{node.vertex, node.dist});
-            if (++cnt == N) break;
-        }
-        return res - max;
     }
+
+    public static int find(int a) {
+        if (parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
+    }
+
+//    public static int prim() {
+//        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+//        pq.offer(new int[]{1, 0});
+//
+//        int max = 0;
+//        int cnt = 0, res = 0;
+//        boolean[] visit = new boolean[N + 1];
+//        int[] curr;
+//        while (!pq.isEmpty()) {
+//            curr = pq.poll();
+//            if (visit[curr[0]]) continue;
+//            visit[curr[0]] = true;
+//            max = Math.max(max, curr[1]);
+//            res += curr[1];
+//            for (Node node : graph[curr[0]])
+//                if (!visit[node.vertex]) pq.offer(new int[]{node.vertex, node.dist});
+//            if (++cnt == N) break;
+//        }
+//        return res - max;
+//    }
 }
