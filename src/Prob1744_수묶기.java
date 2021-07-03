07037018b88508f9a3f75a61afda2bc00f34bd83
danchulee <danchulee@naver.com>
2021-07-03 23:24:44 +0900

    ADD prob1744 ing

---
+++ src/Prob1744_수묶기.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Prob1744_수묶기 {
+    static int N;
+    static Integer[] nums;
+
+    // 음수, 0, 1, 그 외
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        nums = new Integer[N];
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) nums[i] = Integer.parseInt(br.readLine());
+        if (N == 1) answer = nums[0];
+        else {
+            Arrays.sort(nums, (o1, o2) -> {
+                if (o1 > 0 && o2 > 0) return o2 - o1;
+                else return 0;
+            });
+            int index = 0;
+            for (; index < N && nums[index] < 0; index += 2) {
+                if (index == N - 1) {
+                    answer += nums[index];
+                }else if (nums[index + 1] == 0) {
+                    index += 2;
+                    break;
+                } else if (nums[index + 1] > 0) {
+                    answer += nums[index++];
+                    break;
+                } else {
+                    answer += nums[index] * nums[index + 1];
+                }
+            }
+            while (index < N && nums[index] == 0) index++;
+            for (; index < N; index += 2) {
+                if (index == N - 1) {
+                    answer += nums[index];
+                } else if (nums[index] == 1 || nums[index + 1] == 1) {
+                    answer += nums[index] + nums[index + 1];
+                } else answer += nums[index] * nums[index + 1];
+            }
+        }
+        System.out.print(answer);
+    }
+}
---
+++ src/graph/Prob4256_트리.java
@@ -0,0 +1,19 @@
+package graph;
+
+import java.io.*;
+import java.util.*;
+
+public class Prob4256_트리 {
+    static int N;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < T; tc++) {
+            N = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+        }
+    }
+}
